99a09f78716fd123406f3bc797aa903a3e52e665
lazarus <you@example.com>
2025-05-04 17:24:25 +0700

    Add: endpoints for department

---
+++ src/company/router/department.py
@@ -0,0 +1,53 @@
+from typing import Union, AsyncGenerator
+from fastapi import APIRouter, Depends, status, Response
+
+from database import get_session
+from users.models import User
+from users.schemas import UserInformation
+from company.schemas.company import CompanyRead, CompanyCreate
+from company.depencies import get_company_service, get_department_service
+from company.service.department import DepartmentService
+from company.schemas.department import DepartmentRead, DepartmentCreate
+from company.depencies import check_company
+
+department_router = APIRouter(
+    prefix='/companies', tags=['Department operations']
+)
+
+@department_router.post('/{company_id}/departments', response_model=DepartmentRead)
+async def create_department(
+    company_id: int,
+    data: DepartmentCreate,
+
+    session: AsyncGenerator = Depends(get_session),
+    user: User = Depends(check_company),
+    service: DepartmentService = Depends(get_department_service)
+):
+    result = await service.create_department(session, user, company_id, data)
+
+    return DepartmentRead.model_validate(result)
+
+@department_router.patch('/{company_id}/departments/{department_id}', response_model=DepartmentRead)
+async def change_department_head_user(
+    company_id: int,
+    department_id: int,
+    user_id: int,
+    # data: DepartmentCreate,
+
+    session: AsyncGenerator = Depends(get_session),
+    user: User = Depends(check_company),
+    service: DepartmentService = Depends(get_department_service)
+):
+    result = await service.change_head_user(session, user, company_id, department_id, user_id)
+
+    return DepartmentRead.model_validate(result)
+
+@department_router.delete('/{company_id}/departments/{department_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_department(
+    company_id: int,
+    department_id: int,
+    session: AsyncGenerator = Depends(get_session),
+    user: User = Depends(check_company),
+    service: DepartmentService = Depends(get_department_service)
+):
+    await service.delete_department(session, user, company_id, department_id)
