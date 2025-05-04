17517b0d5d089649d94129f42a499386d627bfca
lazarus <you@example.com>
2025-05-04 21:00:02 +0700

    Add: news router

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from users.router import registration_router, auth_router, operation_user
 from company.router.company import company_router
 from company.router.department import department_router
+from news.router import news_router
 
 
 app = FastAPI(
@@ -14,3 +15,4 @@ app.include_router(auth_router, tags=['Auth'])
 app.include_router(operation_user)
 app.include_router(company_router)
 app.include_router(department_router)
+app.include_router(news_router)
