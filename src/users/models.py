4ca1043fa186d9d16d35aad3ce211d4af1eaab45
lazarus <you@example.com>
2025-05-04 17:27:54 +0700

    Fix: change role

--- src/users/models.py
+++ src/users/models.py
@@ -1,10 +1,16 @@
+from typing import TYPE_CHECKING, Optional
 import enum
+
 from fastapi_users.db import SQLAlchemyBaseUserTable
-from sqlalchemy import Boolean, String, Enum, Index
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import Boolean, String, Enum, Index, Integer, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from database import Base
 
+
+if TYPE_CHECKING:
+    from company.models.company import Company
+    from company.models.department import Department
 
 class RoleType(enum.Enum):
     employee = 'employee'
@@ -16,14 +22,29 @@ class User(SQLAlchemyBaseUserTable, Base):
     __tablename__ = 'user'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    first_name: Mapped[str] = mapped_column(String(length=160), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(length=160), nullable=False)
-    role: Mapped[RoleType] = mapped_column(Enum(RoleType), nullable=False)
-    email: Mapped[str] = mapped_column(String(length=320), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(length=40), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(length=40), nullable=False)
+    company_role: Mapped[RoleType] = mapped_column(
+        Enum(RoleType), default=RoleType.employee ,nullable=False
+    )
+    company_id: Mapped[int] = mapped_column(
+        ForeignKey('company.id', ondelete='SET NULL'), nullable=True
+    )
+    department_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey('department.id', ondelete='SET NULL'), nullable=True
+    )
+    email: Mapped[str] = mapped_column(String(length=30), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
-    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
+
+    company: Mapped['Company'] = relationship(
+        back_populates='users', passive_deletes=True
+    )
+    department: Mapped[Optional['Department']] = relationship(
+        back_populates='users', passive_deletes=True, foreign_keys=[department_id]
+    )
 
     __table_args__ = (
         Index('idx_email', 'email'),
