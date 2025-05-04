f3c1307fb6a44c151b0d61f234336d81b7e31419
lazarus <you@example.com>
2025-05-04 17:30:12 +0700

    Add: check user role

---
+++ src/core_depencies.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, HTTPException, status
+
+from users.models import User, RoleType
+from users.manager import fastapi_users
+
+
+def check_role(
+    user: User = Depends(fastapi_users.current_user())
+):
+    if user.company_role != RoleType.admin:
+        print(f'\n\n\n{user.company_role}\n\n\n')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Только админ может использовать функционал оргструктуры'
+        )
+    
+    return user
