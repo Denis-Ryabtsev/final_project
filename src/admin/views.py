42fd63bbcf3ced928f55197f2bc6f890d7cf04a7
lazarus <you@example.com>
2025-05-08 02:56:09 +0700

    Add: create model views for admin site

---
+++ src/admin/views.py
@@ -0,0 +1,24 @@
+from sqladmin import ModelView
+from users.models import User
+from company.models.company import Company
+from tasks.models.task import Task
+
+# class UserAdmin(ModelView, model=User):
+#     column_list = [User.id, User.email, User.first_name]
+
+# class CompanyAdmin(ModelView, model=Company):
+#     column_list = [Company.id, Company.name]
+
+# class TaskAdmin(ModelView, model=Task):
+#     column_list = [Task.id, Task.title, Task.status]
+
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.email, User.first_name, User.last_name, User.company_role]
+
+
+class CompanyAdmin(ModelView, model=Company):
+    column_list = [Company.id, Company.name, Company.company_code]
+
+
+class TaskAdmin(ModelView, model=Task):
+    column_list = [Task.id, Task.title, Task.status, Task.owner_id, Task.target_id]
