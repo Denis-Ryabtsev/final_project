c8462f701d5363f0ae6d21d09958b1e8a8eddf46
lazarus <you@example.com>
2025-05-08 03:00:27 +0700

    Fix: delete old code

--- src/admin/views.py
+++ src/admin/views.py
@@ -3,14 +3,6 @@ from users.models import User
 from company.models.company import Company
 from tasks.models.task import Task
 
-# class UserAdmin(ModelView, model=User):
-#     column_list = [User.id, User.email, User.first_name]
-
-# class CompanyAdmin(ModelView, model=Company):
-#     column_list = [Company.id, Company.name]
-
-# class TaskAdmin(ModelView, model=Task):
-#     column_list = [Task.id, Task.title, Task.status]
 
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.email, User.first_name, User.last_name, User.company_role]
