b57a2463ae120f3ab3c55fc37b6c3d2eea52103f
lazarus <you@example.com>
2025-05-08 02:56:58 +0700

    Add: sqladmin config

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 
 from users.router import registration_router, auth_router, operation_user
 from company.router.company import company_router
@@ -9,9 +10,18 @@ from tasks.router.comment import comment_router
 from rating.router import rating_router
 from meeting.router import meeting_router
 from calendars.router import calendar_router
+from database import db
+from config import get_setting
+from admin.setup import init_admin
 
+
+setting = get_setting()
 
 app = FastAPI(title='Final project')
+# app.add_middleware(SessionMiddleware, secret_key=setting.SECRET_ADMIN)
+
+# Подключение административного кабинета
+init_admin(app, db.engine)
 
 app.include_router(registration_router)
 app.include_router(auth_router, tags=['Auth'])
